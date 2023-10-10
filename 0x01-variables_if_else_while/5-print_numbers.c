1c603e019bdcf45085d21ae732b1dc35d4739dda
edu21n2016 <edu21n2016>
2023-10-09 19:29:45 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/**                                                                                                                                                                           * main - Entry point
-   * Description: 'print numbers of base 10'
-   * Return: always 0
-    */
+/**                                                                                                                                                        
+ * main - Entry point
+ * Descripion: 'print numbers of base 10'
+ * Return: always 0
+ */
 int main(void)
 {
 	int n;
