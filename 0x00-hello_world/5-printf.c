6f158632fbaafbf0d940d04c93b543a0aed1aaea
edu21n2016 <eden22neker@gmail.com>
2023-12-17 00:59:45 -0800

    this code done by beety

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art," followed by a new line
- * Return: 0.
+ * Return: Always 0.
  */
 int main(void)
 {
